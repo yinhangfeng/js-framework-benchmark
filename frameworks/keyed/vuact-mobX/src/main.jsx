ead7d997ac2e2b8291033a5f484975365c5961a0
yinhangfeng <yinhangfeng@bytedance.com>
2026-02-24 21:12:48 +0800

    wip

--- frameworks/keyed/vuact-mobX/src/main.jsx
+++ frameworks/keyed/vuact-mobX/src/main.jsx
@@ -1,3 +1,6 @@
 import 'vuact/setup-scheduler';
 import 'vuact-dom/register-dom-components';
+import { configure } from 'mobx';
 import '../../react-mobX/src/main.jsx';
+
+configure({ reactionScheduler: (fn) => queueMicrotask(fn) });
